1d5cd3dc30085f53e5ae2971cb858c3957520bbc
MatejLauterkranc <m.lauterkranc@gmail.com>
2025-06-08 20:01:18 +0200

    modify

--- Power_BI/Scrap_API_PpenChargPointMap.py
+++ Power_BI/Scrap_API_PpenChargPointMap.py
@@ -3,10 +3,10 @@ import pandas as pd
 import pycountry
 import time
 
-API_KEY = "USE Own API"
+API_KEY = "Your api coude"
 for country in pycountry.countries:
     country_code = country.alpha_2
-    url = f"https://api.openchargemap.io/v3/poi/?output=json&countrycode={country_code}&maxresults=5000&key={API_KEY}"
+    url = f"https://api.openchargemap.io/v3/poi/?output=json&countrycode={country_code}&maxresults=200000&key={API_KEY}"
     try:
         response = requests.get(url)
         if response.status_code == 200:
